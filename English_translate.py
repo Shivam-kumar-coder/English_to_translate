0177a16f6cdf77947580abf595f7c07eb37edb94
coder-shivam <innocentboy9472@gmail.com>
2025-06-21 22:38:38 +0530

    Update English_translate.py

--- English_translate.py
+++ English_translate.py
@@ -2,19 +2,19 @@ import streamlit as st
 from transformers import MarianMTModel, MarianTokenizer
 @st.cache_resource
 def process():
-  global tokenize
-  global model
   tokenize = MarianTokenizer.from_pretrained('Helsinki-NLP/opus-mt-en-hi')
   model = MarianMTModel.from_pretrained('Helsinki-NLP/opus-mt-en-hi')
+  return model 
+  return tokenize
 
 st.title(" Language Translater 🚀")
 i=st.chat_input("enter your Text")
 l=st.selectbox(" Select Your Language Convert ",("hindi"),)
 if i:
-  process()
-  t=tokenize(i,return_tensor='pt',padding=True)
-  m=model.generate(**t)
-  d=tokenize.decode(m,skip_special_token=True)
+  b,s=process()
+  t=s(i,return_tensor='pt',padding=True)
+  m=b.generate(**t)
+  d=s.decode(m,skip_special_token=True)
   st.write(d)
 else:
   st.write("please enter your text")
