baeb2965eff6a3920c926dce60ce182a449c88d0
coder-shivam <innocentboy9472@gmail.com>
2025-06-21 22:09:59 +0530

    Create English_translate.py

---
+++ English_translate.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from transformers import MarianMTModel,MarianTokenizer
+
+@st.cache_resouce
+h_tokenizer=MarianTokenizer.from_pretrained('Helsinki-NLP/opus-mt-en-hi')
+h_model=MarianMTModel.from_pretrained('Helsinki-NLP/opus-mt-en-hi')
+
+st.title(" Language Translater 🚀")
+i=st.chat_input("enter your Text")
+l=st.selectbox(" Select Your Language Convert ")
+if i:
+  t=h_tokenizer(i,return_tensor='pt',padding=True)
+  m=h_model.generate(**t)
+  d=h_tokenizer.decode(m,skip_special_token=True)
+  st.write(d)
