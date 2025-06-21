bdf4932c0fa7f248121618d2087120cf5c2b3380
coder-shivam <innocentboy9472@gmail.com>
2025-06-21 23:10:53 +0530

    Update English_translate.py

--- English_translate.py
+++ English_translate.py
@@ -4,17 +4,17 @@ from transformers import MarianMTModel, MarianTokenizer
 def process():
   tokenize = MarianTokenizer.from_pretrained('Helsinki-NLP/opus-mt-en-hi')
   model = MarianMTModel.from_pretrained('Helsinki-NLP/opus-mt-en-hi')
-  return model 
-  return tokenize
+  return model, tokenize
+
 
 st.title(" Language Translater 🚀")
 i=st.chat_input("enter your Text")
 l=st.selectbox(" Select Your Language Convert ",("hindi"),)
 if i:
-  model,tokenize=process()
-  t=tokenize(i,return_tensor='pt',padding=True)
-  m=model.generate(**t)
-  d=tokenize.decode(m,skip_special_tokens=True)
+  mode,tokeniz=process()
+  t=tokeniz(i,return_tensor='pt',padding=True)
+  m=mode.generate(**t)
+  d=tokeniz.decode(m,skip_special_tokens=True)
   st.write(d)
 else:
   st.write("please enter your text")
